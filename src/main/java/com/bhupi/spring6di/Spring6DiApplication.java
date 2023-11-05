1bb15293ac8842923c5c02498c993bd4d13dc21a
Bhupendra Singh <bhupi29491@gmail.com>
2023-11-05 20:42:41 +0200

    Spring context implementation

--- src/main/java/com/bhupi/spring6di/Spring6DiApplication.java
+++ src/main/java/com/bhupi/spring6di/Spring6DiApplication.java
@@ -1,13 +1,23 @@
 package com.bhupi.spring6di;
 
+import com.bhupi.spring6di.controllers.MyController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class Spring6DiApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Spring6DiApplication.class, args);
+
+		ApplicationContext ctx = SpringApplication.run(Spring6DiApplication.class, args);
+
+		MyController controller = ctx.getBean(MyController.class);
+
+		System.out.println("I am in Main Method");
+
+		System.out.println(controller.sayHello());
 	}
 
 }
