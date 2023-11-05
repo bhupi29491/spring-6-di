5119110117cce46d57b937a03926e1cb832da2ef
Bhupendra Singh <bhupi29491@gmail.com>
2023-11-05 22:43:13 +0200

    Configured Spring profiles in the application

---
+++ src/main/java/com/bhupi/spring6di/services/i18n/EnglishGreetingService.java
@@ -0,0 +1,15 @@
+package com.bhupi.spring6di.services.i18n;
+
+import com.bhupi.spring6di.services.GreetingService;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+@Profile("EN")
+@Service("i18NService")
+public class EnglishGreetingService implements GreetingService {
+
+    @Override
+    public String sayGreeting() {
+        return "Hello World - EN";
+    }
+}
