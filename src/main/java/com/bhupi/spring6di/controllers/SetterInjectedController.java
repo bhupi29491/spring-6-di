cc8f103150628c2a5fe3483575d2a87d7c09baf8
Bhupendra Singh <bhupi29491@gmail.com>
2023-11-05 21:54:39 +0200

    Using qualifiers to inform Spring for Beans

--- src/main/java/com/bhupi/spring6di/controllers/SetterInjectedController.java
+++ src/main/java/com/bhupi/spring6di/controllers/SetterInjectedController.java
@@ -2,6 +2,7 @@ package com.bhupi.spring6di.controllers;
 
 import com.bhupi.spring6di.services.GreetingService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 
 @Controller
@@ -11,7 +12,7 @@ public class SetterInjectedController {
     private GreetingService greetingService;
 
     @Autowired
-    public void setGreetingService(GreetingService greetingService) {
+    public void setGreetingService(@Qualifier("setterGreetingBean")GreetingService greetingService) {
         this.greetingService = greetingService;
     }
 
